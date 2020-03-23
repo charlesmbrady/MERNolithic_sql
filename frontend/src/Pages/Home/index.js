6eaf564f18118a4ef1a61461dda19d411fdbfacf
Charles <charlesmbrady@gmail.com>
2020-03-23 01:37:33 -0400

    add is healthy button

--- frontend/src/Pages/Home/index.js
+++ frontend/src/Pages/Home/index.js
@@ -1,24 +1,24 @@
 import './style.css';
-import React from 'react';
+import React, { useState } from 'react';
 import API from '../../Utilities/API';
 
 export default function Home() {
+  const [healthy, setHealthy] = useState(false);
   const healthcheck = () => {
     API.healthcheck(res => {
       if (res) {
         console.log('app is healthy');
+        setHealthy(true);
       }
     });
   };
 
-  // const aTestFunctionForCoverage = () => {
-  //   console.log('testsingggggg yaa');
-  // };
-
   return (
     <div>
-      Healthcheck asdf(see console).
-      <button onClick={() => healthcheck()}>Get server response...</button>
+      <h1>Healthcheck</h1>
+      <button onClick={() => healthcheck()}>ping</button>
+
+      {healthy && <div>App is healthy!</div>}
     </div>
   );
 }
