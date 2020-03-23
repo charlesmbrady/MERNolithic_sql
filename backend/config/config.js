b5824a99ae742d08edbe592daee58def375eda0d
Charles <charlesmbrady@gmail.com>
2020-03-23 00:00:45 -0400

    added cypress testing for different environments

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.SEQUELIZE_USER,
-    password: process.env.SEQUELIZE_PASSWORD,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: 'mernolithic_dev',
     details: {
-      host: process.env.SEQUELIZE_HOST,
+      host: 'localhost',
       port: 3306,
       dialect: 'mysql'
     }
@@ -17,17 +17,14 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD,
     database: 'mernolithic_test',
     details: {
-      host: 'localhost',
+      host: '127.0.0.1', // use 'localhost' if go back to using this step in CI
       port: 3306,
       dialect: 'mysql'
     }
   },
   production: {
-    // use_env_variable: 'JAWSDB_MARIA_URL',
     use_env_variable: 'JAWSDB_URL',
 
-    // url:
-    //   'mysql://oku92haxwe074gx6:pt7uvw59en726iba@u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pg69vfez19z9ktej',
     details: {
       host: 'u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
       username: 'oku92haxwe074gx6',
