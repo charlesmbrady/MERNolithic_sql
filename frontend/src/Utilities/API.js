35f57c33edae9256ddb22a0bf86f0162de1aa1d0
Charles <charlesmbrady@gmail.com>
2020-05-10 12:45:29 -0400

    remove extra api endpoint

--- frontend/src/Utilities/API.js
+++ frontend/src/Utilities/API.js
@@ -1,19 +1,14 @@
 import axios from 'axios';
 
 export default {
-  healthcheck: function() {
-    return axios.get('/healthcheck').catch(err => {
+  healthcheck: function () {
+    return axios.get('/healthcheck').catch((err) => {
       throw err;
     });
   },
-  checkDatabase: function() {
-    return axios.get('/auth/users').catch(err => {
+  createUser: function () {
+    return axios.post('/auth/users').catch((err) => {
       throw err;
     });
   },
-  createUser: function() {
-    return axios.post('/auth/users').catch(err => {
-      throw err;
-    });
-  }
 };
