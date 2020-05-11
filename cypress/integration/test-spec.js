6f540e36f931e44b2d91a34e3b6ddf4465c9b55b
Charles <charlesmbrady@gmail.com>
2020-05-11 14:01:40 -0400

    update user create test

--- cypress/integration/test-spec.js
+++ cypress/integration/test-spec.js
@@ -1,26 +1,26 @@
 /// <reference types="cypress" />
 
-describe('home', function() {
-  it('can click ping button', function() {
+describe('home', function () {
+  it('can click ping button', function () {
     cy.visit('/');
     cy.get('button').click();
   });
 });
 
-describe('Users', function() {
-  it('Canary test', function() {
+describe('Users', function () {
+  it('Canary test', function () {
     expect(1).to.equal(1);
   });
 
-  it('Can create user', function() {
+  it('Can create user', function () {
     // cy.visit('/');
 
-    cy.request('POST', `${Cypress.config('apiUrl')}/auth/register`, {
+    cy.request('POST', `${Cypress.config('apiUrl')}/auth/user`, {
       firstName: 'charles',
       lastName: 'brady',
       email: 'ranom',
-      password: 'myPassword1!'
-    }).then(response => {
+      password: 'myPassword1!',
+    }).then((response) => {
       // response.body is automatically serialized into JSON
       expect(response.body).to.have.property('firstName', 'charles'); // true
     });
