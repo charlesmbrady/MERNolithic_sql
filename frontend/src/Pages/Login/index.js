b172e29145231673d8d7357f93e52eb7d3250e80
Charles <charlesmbrady@gmail.com>
2020-05-15 13:32:41 -0400

    removed comments

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -65,67 +65,6 @@ export default function Login() {
         <Form title='Login' />
         {fields}
         <FormFooter formFooterItems={formFooterItems} />
-        {/* <form
-          id='form'
-          className={style.form}
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <h2 data-test='header' className={style.title}>
-            Login
-          </h2>
-          <div className={style.control}>
-            <label className={style.label} for='email' data-test='label-email'>
-              Email
-            </label>
-            <input
-              className={style.input}
-              type='text'
-              id='email'
-              data-test='login-input-email'
-              placeholder='Enter email'
-              name='email'
-              value={newUser.email}
-              onChange={(e) => formUpdate(e.target.name, e.target.value)}
-            />
-            <small className={style.error} data-test='error-email'>
-              Error message
-            </small>
-          </div>
-          <div className={style.control}>
-            <label
-              className={style.label}
-              for='password'
-              data-test='label-password'
-            >
-              Password
-            </label>
-            <input
-              className={style.input}
-              type='password'
-              id='password'
-              data-test='login-input-password'
-              placeholder='Enter password'
-              name='password'
-              value={newUser.password}
-              onChange={(e) => formUpdate(e.target.name, e.target.value)}
-            />
-            <small className={style.error} data-test='error-password'>
-              Error message
-            </small>
-          </div>
-          <button
-            type='submit'
-            data-test='login-submit-button'
-            onClick={() => authenticateUser(newUser)}
-          >
-            Submit
-          </button>
-          <small className={style.option}>
-            <Link to='/register' data-test='login-to-register'>
-              Don't have an account yet?
-            </Link>
-          </small>
-        </form> */}
       </div>
     </div>
   );
