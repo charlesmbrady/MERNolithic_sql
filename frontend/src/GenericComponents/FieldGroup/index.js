065818ad5ea79451998f136cd03ba176f77fbf5d
Charles <charlesmbrady@gmail.com>
2020-05-15 13:32:24 -0400

    refactored with authentication working for existing user

--- frontend/src/GenericComponents/FieldGroup/index.js
+++ frontend/src/GenericComponents/FieldGroup/index.js
@@ -1,13 +1,26 @@
 import style from './style.css';
-import React from 'react';
+import React, { useContext } from 'react';
+import { FormContext } from '../../Contexts/FormContext';
 
 export default function FieldGroup({ type, label, name, placeholder }) {
+  const { form, setForm } = useContext(FormContext);
+
+  const formUpdate = (fieldName, value) => {
+    let tempForm = { ...form };
+    tempForm[fieldName].input = value;
+    setForm(tempForm);
+  };
+
   return (
     <div className={style.fieldGroup}>
-      <label htmlFor='' className={style.label}>
-        Field Label
-      </label>
-      <input type='text' className={style.input} />
+      <label className={style.label}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        className={style.input}
+        value={form[name].input}
+        onChange={(e) => formUpdate(e.target.name, e.target.value)}
+      />
       <small className={style.error}>Put error here</small>
     </div>
   );
--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -7,27 +7,22 @@ import API from '../../Utilities/API';
 
 //Form Components
 import Form from '../../GenericComponents/Form';
-import FormHeader from '../../GenericComponents/FormHeader';
 import FormFooter from '../../GenericComponents/FormFooter';
 import SubmitButton from '../../GenericComponents/SubmitButton';
 import FieldGroup from '../../GenericComponents/FieldGroup';
 
 export default function Login() {
   const [userAuthenticated, setUserAuthenticated] = useState(false);
-  const [newUser, setNewUser] = useState({
-    email: null,
-    password: null,
-  });
+
   const { user, setUser } = useContext(UserContext);
 
-  const formUpdate = (fieldName, value) => {
-    let tempUser = { ...newUser };
-    tempUser[fieldName] = value;
-
-    setNewUser(tempUser);
+  const { form, setForm } = useContext(FormContext);
+  const newUser = {
+    email: form.email.input,
+    password: form.password.input,
   };
 
-  const authenticateUser = (newUser) => {
+  const authenticateUser = () => {
     API.authenticate(newUser).then((res) => {
       if (res.status === 200) {
         setUser({ ...user, isAuthenticated: true });
@@ -40,11 +35,37 @@ export default function Login() {
     return <Redirect to='/dashboard' />;
   }
 
+  const fields = [
+    <FieldGroup
+      type='text'
+      label='Email'
+      name='email'
+      placeholder='Enter email address'
+    />,
+    <FieldGroup
+      type='password'
+      label='Password'
+      name='password'
+      placeholder='Enter password'
+    />,
+  ];
+
+  const formFooterItems = [
+    <SubmitButton text='Submit' submitFunction={() => authenticateUser()} />,
+    <small className={style.option}>
+      <Link to='/register' data-test='login-to-register'>
+        Don't have an account yet?
+      </Link>
+    </small>,
+  ];
+
   return (
     <div className={style.gridContainer}>
       <div className={style.container}>
         <Form title='Login' />
-        <form
+        {fields}
+        <FormFooter formFooterItems={formFooterItems} />
+        {/* <form
           id='form'
           className={style.form}
           onSubmit={(e) => e.preventDefault()}
@@ -104,7 +125,7 @@ export default function Login() {
               Don't have an account yet?
             </Link>
           </small>
-        </form>
+        </form> */}
       </div>
     </div>
   );
