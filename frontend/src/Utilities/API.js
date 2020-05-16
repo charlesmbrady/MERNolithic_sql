c988ec78fb33e0c50f11e953c799bad4e84b2c08
Charles <charlesmbrady@gmail.com>
2020-05-16 05:24:34 -0400

    removed healthcheck api call

--- frontend/src/Utilities/API.js
+++ frontend/src/Utilities/API.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
 export default {
-  healthcheck: function () {
-    return axios.get('/healthcheck').catch((err) => {
-      throw err;
-    });
-  },
   createUser: function (user) {
     return axios.post('/auth/user', user).catch((err) => {
       throw err;
