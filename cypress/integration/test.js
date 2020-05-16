0faabbc1484b911ff84c043570b5a926d9e5273e
Charles <charlesmbrady@gmail.com>
2020-05-16 05:13:06 -0400

    added new user and existing user tests

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,3 +1,22 @@
 /// <reference types="cypress" />
 
-describe('Users', function () {});
+describe('Users', function () {
+  it('Can create user', () => {
+    cy.visit('/');
+    cy.get('[data-test="register-navlink"]').click();
+    cy.get('[data-test="firstName-input"]').type('Charles');
+    cy.get('[data-test="lastName-input"]').type('Charles');
+    cy.get('[data-test="email-input"]').type('charlesmbrady@gmail.com');
+    cy.get('[data-test="password-input"]').type('Password1!');
+    cy.get('[data-test="passwordConfirmation-input"]').type('Password1!');
+    cy.get('[data-test="submit-button"]').click();
+  });
+
+  it('Can login with existing user', () => {
+    cy.visit('/');
+    cy.get('[data-test="login-navlink"]').click();
+    cy.get('[data-test="email-input"]').type('charlesmbrady@gmail.com');
+    cy.get('[data-test="password-input"]').type('Password1!');
+    cy.get('[data-test="submit-button"]').click();
+  });
+});
