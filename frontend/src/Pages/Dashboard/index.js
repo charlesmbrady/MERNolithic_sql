255307a934c77635c37f3d78ba901e5d71b624dc
Charles <charlesmbrady@gmail.com>
2020-05-16 05:25:17 -0400

    removed healthcheck call

--- frontend/src/Pages/Dashboard/index.js
+++ frontend/src/Pages/Dashboard/index.js
@@ -3,18 +3,9 @@ import React from 'react';
 import API from '../../Utilities/API';
 
 export default function Dashboard() {
-  const healthcheck = () => {
-    API.healthcheck((res) => {
-      if (res) {
-        console.log('app is healthy');
-      }
-    });
-  };
-
   return (
     <div>
-      <h1>Healthcheck</h1>
-      <button onClick={() => healthcheck()}>ping</button>
+      <h1>Dashboard stuff</h1>
     </div>
   );
 }
