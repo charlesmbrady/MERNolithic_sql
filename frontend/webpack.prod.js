292f9aa3863c1c13bdbd033129c182d41c8d065a
Charles <charlesmbrady@gmail.com>
2020-05-22 18:00:05 -0400

    added webpack bundleAnalyzerPlugin

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,7 +1,14 @@
 const merge = require('webpack-merge');
+const path = require('path');
 const common = require('./webpack.common.js');
+require('dotenv').config(path.join(__dirname, '../.env'));
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+console.log(process.env.BUNDLE_ANALYZE);
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins:
+    process.env.BUNDLE_ANALYZE == true ? [new BundleAnalyzerPlugin()] : [],
 });
