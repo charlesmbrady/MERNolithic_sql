b0d61743daacd9878ce6b93f210150fd0e186982
Charles <charlesmbrady@gmail.com>
2020-05-23 02:17:40 -0400

    try new setup to persist env variables

--- setup-env.sh
+++ setup-env.sh
@@ -1,11 +1,11 @@
 case $CIRCLE_BRANCH in
     "develop")
         export ENVIRONMENT="dev"
-        export HEROKU_APP="mernolithic-staging"
+        export HEROKU_APP="mernolithic-staging" >> "$BASH_ENV"
         ;;
     "master")
         export ENVIRONMENT="production"
-        export HEROKU_APP="mernolithic"
+        export HEROKU_APP="mernolithic" >> "$BASH_ENV"
         ;;
 esac
 export NODE_ENV="production"
