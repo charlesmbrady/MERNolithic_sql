e9f6b370ccbdb25da15800fec280b9c2e3c3fca9
Charles <charlesmbrady@gmail.com>
2020-05-22 20:58:03 -0400

    remove unused route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const primaryItemsRoutes = require('./primaryItems');
-const testsRoutes = require('./tests');
 
 router.use(primaryItemsRoutes);
-router.use(testsRoutes);
 
 module.exports = router;
