ebcf3959189e0f7e096183e9d7b1515cebe8a3ac
Charles <charlesmbrady@gmail.com>
2020-05-23 23:53:26 -0400

    improved cypress tests with new User constructor function

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,19 +1,22 @@
 /// <reference types="cypress" />
+import User from '../helpers/newUser';
 
 import Nav from '../elements/nav';
 import Login from '../elements/login';
 import Register from '../elements/register';
 
 describe('User Authentication', function () {
+  const userOne = new User();
   it('User can register for new account', () => {
+    console.log(userOne);
     cy.visit('/');
     cy.get(Nav.REGISTER).click();
     cy.url().should('include', '/register');
-    cy.get(Register.FIRST_NAME).type('Charles');
-    cy.get(Register.LAST_NAME).type('Brady');
-    cy.get(Register.EMAIL).type('charlesmbrady@gmail.com');
-    cy.get(Register.PASSWORD).type('Password1!');
-    cy.get(Register.PASSWORD_CONFIRMATION).type('Password1!');
+    cy.get(Register.FIRST_NAME).type(userOne.firstName);
+    cy.get(Register.LAST_NAME).type(userOne.lastName);
+    cy.get(Register.EMAIL).type(userOne.email);
+    cy.get(Register.PASSWORD).type(userOne.password);
+    cy.get(Register.PASSWORD_CONFIRMATION).type(userOne.passwordConfirmation);
     cy.get(Register.SUBMIT).click();
     // cy.url().should('include', '/login');
   });
@@ -22,8 +25,8 @@ describe('User Authentication', function () {
     cy.visit('/');
     cy.get(Nav.LOGIN).click();
     cy.url().should('include', '/login');
-    cy.get(Login.EMAIL).type('charlesmbrady@gmail.com');
-    cy.get(Login.PASSWORD).type('Password1!');
+    cy.get(Login.EMAIL).type(userOne.email);
+    cy.get(Login.PASSWORD).type(userOne.password);
     cy.get(Login.SUBMIT).click();
     // cy.url().should('include', '/dashboard');
   });
@@ -31,25 +34,19 @@ describe('User Authentication', function () {
   it('Can logout', () => {
     cy.visit('/');
     cy.get(Nav.LOGIN).click();
-    cy.get(Login.EMAIL).type('charlesmbrady@gmail.com');
-    cy.get(Login.PASSWORD).type('Password1!');
+    cy.get(Login.EMAIL).type(userOne.email);
+    cy.get(Login.PASSWORD).type(userOne.password);
     cy.get(Login.SUBMIT).click();
     cy.get(Nav.LOGOUT).click();
   });
 
-  // it('Authenticated user will be redirected from homepage to projects on homepage load', () => {
-  //   const user = {
-  //     firstName: 'Charles',
-  //     lastName: 'Brady',
-  //     email: 'chazzyb@gmail.com',
-  //     password: 'Password1!',
-  //     passwordConfirmation: 'Password1!',
-  //   };
+  it('Authenticated user will be directed to dashboard after successful login', () => {
+    const userTwo = new User();
 
-  //   cy.registerNewUser(user);
-  //   cy.login(user);
-  //   cy.url().should('contain', 'projects');
-  // });
+    cy.registerNewUser(userTwo);
+    cy.login(userTwo);
+    cy.url().should('contain', 'dashboard');
+  });
 
   it('Non-authenticated user will be redirect to login from protected route', () => {
     cy.visit('/dashboard');
@@ -58,22 +55,23 @@ describe('User Authentication', function () {
 });
 
 describe('API - Authentication', () => {
+  const userThree = new User();
   it('Can create user', function () {
-    cy.request('POST', `${Cypress.config('apiUrl')}/auth/user`, {
-      firstName: 'charles',
-      lastName: 'brady',
-      email: 'ranom',
-      password: 'myPassword1!',
-    }).then((response) => {
-      // response.body is automatically serialized into JSON
-      expect(response.body).to.have.property('firstName', 'charles'); // true
-    });
+    cy.request('POST', `${Cypress.config('apiUrl')}/auth/user`, userThree).then(
+      (response) => {
+        // response.body is automatically serialized into JSON
+        expect(response.body).to.have.property(
+          'firstName',
+          userThree.firstName
+        ); // true
+      }
+    );
   });
 
   it('Can authenticate user', function () {
     cy.request('POST', `${Cypress.config('apiUrl')}/auth/authenticate`, {
-      email: 'ranom',
-      password: 'myPassword1!',
+      email: userThree.email,
+      password: userThree.password,
     }).then((response) => {
       expect(response.body).to.equal('OK');
     });
