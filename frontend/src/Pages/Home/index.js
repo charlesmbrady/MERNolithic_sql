b54ba3fcbb7b75e76bf502fd74c313bb54bac35f
Charles <charlesmbrady@gmail.com>
2020-05-23 22:01:02 -0400

    updated footer on home page. Turned off deploy step for now

--- frontend/src/Pages/Home/index.js
+++ frontend/src/Pages/Home/index.js
@@ -3,32 +3,27 @@ import React from 'react';
 import Hero from '../../GenericComponents/Hero';
 
 export default function Home() {
-  const mainItemTwo = (
-    <h3 className='mernolithic-info'>
-      This project was made with the{' '}
-      <a
-        href='https://github.com/charlesmbrady/MERNolithic_sql'
-        target='_blank'
-      >
-        MERNolithic
-      </a>{' '}
-      starter project <br></br> developed by Charles Brady.
-    </h3>
-  );
-  const subItemTwo = <p></p>;
-  const mainItem = (
-    <h2 className='head'>
-      <a href='https://github.com/charlesmbrady/MERNolithic_sql'>Get Started</a>{' '}
-      with MERNolithic!
-    </h2>
-  );
-  const subItem = <div></div>;
-
   return (
     <div className='homeContainer'>
-      <Hero mainItem={mainItemTwo} subItem={subItemTwo} />
-
-      <Hero mainItem={mainItem} subItem={subItem} />
+      <div className='contentContainer'></div>
+      <footer className='footer'>
+        <h3 className='mernolithic-info'>
+          This project was made with the{' '}
+          <a
+            href='https://github.com/charlesmbrady/MERNolithic_sql'
+            target='_blank'
+          >
+            MERNolithic
+          </a>{' '}
+          starter project, developed by Charles Brady.
+        </h3>
+        <h2 className='head'>
+          <a href='https://github.com/charlesmbrady/MERNolithic_sql'>
+            Get Started
+          </a>{' '}
+          with MERNolithic!
+        </h2>
+      </footer>
     </div>
   );
 }
